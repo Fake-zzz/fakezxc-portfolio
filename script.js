35ec3520a313fe5b98444bcc350772542d0cb9c6
Fake-zzz <109675435+Fake-zzz@users.noreply.github.com>
2026-04-24 12:29:08 -0300

    v1.9.3

--- script.js
+++ script.js
@@ -28,10 +28,11 @@ let carouselIndex = 0;
 let portfolioVideos = [
   { type: 'youtube', url: 'https://youtu.be/eP33HKyUKOU', section: 'portfolio' },
   { type: 'youtube', url: 'https://youtu.be/ROk8W1-p0o0', section: 'portfolio' },
+  { type: 'youtube', url: 'https://youtu.be/5-NWTJOtW5s', section: 'portfolio' },
   { type: 'youtube', url: 'https://youtu.be/SpKBJxTKNjU', section: 'portfolio' },
   { type: 'youtube', url: 'https://youtu.be/Co7QfOb1NQ4', section: 'portfolio' },
-  { type: 'youtube', url: 'https://youtu.be/mJ6uk5oh30Y?t=5', section: 'portfolio' },
   { type: 'youtube', url: 'https://youtu.be/tYbc-T-UYng', section: 'portfolio' },
+  { type: 'youtube', url: 'https://youtu.be/mJ6uk5oh30Y?t=5', section: 'portfolio' },
   { type: 'youtube', url: 'https://youtu.be/GqAkNSdwUuM', section: 'portfolio' },
   {type:  'youtube', url: 'https://youtu.be/Um4WwpMpDdE', thumb: 'images/thumb3.png', section: 'portfolio'},
   { type: 'youtube', url: 'https://youtu.be/klBS2gSbKws', section: 'portfolio' },
